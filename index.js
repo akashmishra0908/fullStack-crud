87c35637849b2ea6abfc5c0ebb4f936275b4ee80
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-09-25 16:42:04 +0530

    backend completed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const connection=require("./config/db");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app=express();
+app.use(cors());
 app.use(express.json());
 const userRouter=require("./routes/userRoutes");
 const postRouter=require("./routes/postRoutes");
